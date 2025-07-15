62f67396fed48762d7cb493f2c4988da7fd77598
Cristian Marcelo Quenguan Malte <141966711+crismaque@users.noreply.github.com>
2025-07-15 00:16:44 -0500

    Update conv_tts.h

--- src/conv_tts.h
+++ src/conv_tts.h
@@ -1,8 +1,11 @@
 #ifndef CONV_TTS_H
 #define CONV_TTS_H
 
+#include <sys/types.h>
+
 void speak_line(const char *text, const char *lang, int speed);
-void stop_speech(void);
 int is_speaking(void);
+void stop_speech(void);
+extern pid_t espeak_pid;
 
 #endif
