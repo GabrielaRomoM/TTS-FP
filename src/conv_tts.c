ee31f87b00d0d601e96177d579b5849faecd9b47
Cristian Marcelo Quenguan Malte <141966711+crismaque@users.noreply.github.com>
2025-07-15 00:17:06 -0500

    Update conv_tts.c

--- src/conv_tts.c
+++ src/conv_tts.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 #include "conv_tts.h"
 
 pid_t espeak_pid = -1;
 
 void speak_line(const char *text, const char *lang, int speed) {
+    if (espeak_pid > 0) {
+        waitpid(espeak_pid, NULL, 0);  // Espera al anterior si no ha terminado
+    }
+
     espeak_pid = fork();
     if (espeak_pid == 0) {
-        // Proceso hijo
-        char speed_str[10];
-        snprintf(speed_str, sizeof(speed_str), "%d", speed);
-        execlp("espeak", "espeak", "-v", lang, "-s", speed_str, text, NULL);
-        // Si falla
-        perror("execlp");
+        char command[512];
+        snprintf(command, sizeof(command), "espeak -v %s -s %d \"%s\"", lang, speed, text);
+        execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+        perror("execl falló");
         exit(1);
     }
 }
 
-int is_speaking() {
+int is_speaking(void) {
     if (espeak_pid <= 0) return 0;
+
     int status;
     pid_t result = waitpid(espeak_pid, &status, WNOHANG);
-    return result == 0; // aún está corriendo
+    return (result == 0);  // Sigue hablando si no ha terminado
 }
 
-void stop_speech() {
+void stop_speech(void) {
     if (espeak_pid > 0) {
         kill(espeak_pid, SIGKILL);
         waitpid(espeak_pid, NULL, 0);
