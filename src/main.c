a85d1e0f5f65df4a038da81c30d59b39e13c5b16
Cristian Marcelo Quenguan Malte <141966711+crismaque@users.noreply.github.com>
2025-07-15 07:33:38 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,14 +4,14 @@
 #include "config.h"
 #include "loader.h"
 #include "conv_tts.h"
-#include <signal.h>     // Para SIGSTOP, SIGCONT
-#include <unistd.h>     // Para pid_t
+#include <signal.h>
+#include <unistd.h>
 
-int is_speaking(void);
-void stop_speech(void);
+#define BTN_PAUSE   4   // GPIO23
+#define BTN_STOP    5   // GPIO24
+#define BTN_REPEAT  6   // GPIO25
+#define BTN_RECONF  7   // GPIO26
 
-#define BTN_PAUSE  4  // GPIO23
-#define BTN_STOP   5  // GPIO24
 extern pid_t espeak_pid;
 
 int main() {
@@ -26,51 +26,85 @@ int main() {
         }
     }
 
-    int line_count = 0;
-    char **text = load_text(config.path, &line_count);
-    if (!text || line_count == 0) {
-        printf("Error: archivo vacío o no válido.\n");
-        return 1;
-    }
-
-    // Configura botones
     wiringPiSetup();
     pinMode(BTN_PAUSE, INPUT);
     pinMode(BTN_STOP, INPUT);
+    pinMode(BTN_REPEAT, INPUT);
+    pinMode(BTN_RECONF, INPUT);
+
     pullUpDnControl(BTN_PAUSE, PUD_UP);
     pullUpDnControl(BTN_STOP, PUD_UP);
+    pullUpDnControl(BTN_REPEAT, PUD_UP);
+    pullUpDnControl(BTN_RECONF, PUD_UP);
 
-    int paused = 0;
+    int repetir = 0;
 
-    for (int i = 0; i < line_count; ++i) {
-        speak_line(text[i], config.language, config.speed);
+    do {
+        int paused = 0;
+        int line_count = 0;
+        char **text = load_text(config.path, &line_count);
+        if (!text || line_count == 0) {
+            printf("Error: archivo vacío o no válido.\n");
+            return 1;
+        }
 
-        while (is_speaking()) {
-            if (digitalRead(BTN_STOP) == LOW) {
-                stop_speech();
-                printf(">> Reproducción detenida\n");
-                goto end;
+        for (int i = 0; i < line_count; ++i) {
+            speak_line(text[i], config.language, config.speed);
+            while (is_speaking()) {
+                if (digitalRead(BTN_STOP) == LOW) {
+                    stop_speech();
+                    printf(">> Reproducción detenida\n");
+                    goto fin_reproduccion;
+                }
+
+                if (digitalRead(BTN_PAUSE) == LOW) {
+                    delay(200);
+                    while (digitalRead(BTN_PAUSE) == LOW);
+                    paused = !paused;
+                    if (paused) {
+                        printf(">> Pausado\n");
+                        kill(espeak_pid, SIGSTOP);
+                    } else {
+                        printf(">> Reanudado\n");
+                        kill(espeak_pid, SIGCONT);
+                    }
+                }
+                delay(100);
+            }
+        }
+
+    fin_reproduccion:
+        free_text(text, line_count);
+
+        printf("\n=== FIN ===\n");
+        printf("BTN_REPEAT (GPIO25) → Repetir\n");
+        printf("BTN_RECONF (GPIO26) → Nueva configuración\n");
+
+        repetir = 0;
+        while (1) {
+            if (digitalRead(BTN_REPEAT) == LOW) {
+                delay(200);
+                while (digitalRead(BTN_REPEAT) == LOW);
+                repetir = 1;
+                break;
             }
 
-            if (digitalRead(BTN_PAUSE) == LOW) {
-                delay(200); // anti-rebote
-                while (digitalRead(BTN_PAUSE) == LOW);
-                paused = !paused;
-
-                if (paused) {
-                    printf(">> Pausado\n");
-                    kill(espeak_pid, SIGSTOP); // Pausar proceso
-                } else {
-                    printf(">> Reanudado\n");
-                    kill(espeak_pid, SIGCONT); // Reanudar proceso
+            if (digitalRead(BTN_RECONF) == LOW) {
+                delay(200);
+                while (digitalRead(BTN_RECONF) == LOW);
+                run_config_assistant();
+                if (load_config(&config) != 0) {
+                    printf("Error al leer la nueva configuración.\n");
+                    return 1;
                 }
+                repetir = 1;
+                break;
             }
 
             delay(100);
         }
-    }
 
-end:
-    free_text(text, line_count);
+    } while (repetir);
+
     return 0;
 }
