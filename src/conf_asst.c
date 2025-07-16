1f911c6e2fd993ef85c530942f5d2a6873c306d1
Cristian Marcelo Quenguan Malte <141966711+crismaque@users.noreply.github.com>
2025-07-15 23:31:14 -0500

    Update conf_asst.c

--- src/conf_asst.c
+++ src/conf_asst.c
@@ -96,8 +96,7 @@ void run_config_assistant(void) {
     if (file) {
         fprintf(file, "path=%s\nlanguage=%s\nspeed=%d\n", path, idiomas[idioma_idx], speeds[speed_idx]);
         fclose(file);
-        printf("\nConfiguración guardada.\n");
-        printf("\nReproduciendo...");
+        printf("\nConfiguración guardada.\nReproduciendo...");
     } else {
         perror("Error al guardar configuración");
     }
